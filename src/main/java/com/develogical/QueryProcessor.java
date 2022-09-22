28aa3a0958734e76abfa03db1c4e66187eda8774
fionaaac <cchiu2@andrew.cmu.edu>
2022-09-22 10:33:33 -0400

    changed query to what is your name

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,10 +3,11 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
+        if (query.toLowerCase().contains("what is your name")) {
+            return "hello";
+            // return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+            //         "English poet, playwright, and actor, widely regarded as the greatest " +
+            //         "writer in the English language and the world's pre-eminent dramatist.";
         }
         return "Could not process query";
     }
