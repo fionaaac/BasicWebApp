793a2d05bc1f81d550e67636a5bb095458ae1331
fionaaac <cchiu2@andrew.cmu.edu>
2022-09-22 11:05:37 -0400

    added plus query handling

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,12 +3,33 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("what is your name")) {
-            return "hello";
-            // return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-            //         "English poet, playwright, and actor, widely regarded as the greatest " +
-            //         "writer in the English language and the world's pre-eminent dramatist.";
+        if (query.toLowerCase().contains("shakespeare")) {
+            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+                    "English poet, playwright, and actor, widely regarded as the greatest " +
+                    "writer in the English language and the world's pre-eminent dramatist.";
         }
+        else if (query.toLowerCase().contains("plus")) {
+            String[] parts = query.split(" ");
+            // System.out.println(parts);
+            return String.valueOf((Integer.parseInt(parts[2]) + Integer.parseInt(parts[4])));
+        }
+        // else if (query.toLowerCase().contains("which of the following numbers is the largest")) {
+        //     String[] parts = query.split("%20");
+        //     String[] numbers = parts[7].split("%");
+
+        //     int i;
+                
+        //     // Initialize maximum element
+        //     int max = Integer.parseInt(numbers[1]);
+                
+        //     // Traverse array elements from second and
+        //     // compare every element with current max
+        //     for (i = 1; i < numbers.length; i++)
+        //         if (Integer.parseInt(numbers[i]) > max)
+        //             max = numbers[i];
+                
+        //     return max;
+        // }
         return "Could not process query";
     }
 }
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -15,14 +15,24 @@ public class QueryProcessorTest {
         assertThat(queryProcessor.process("test"), is("Could not process query"));
     }
 
-    @Test
-    public void knowsAboutShakespeare() throws Exception {
-        assertThat(queryProcessor.process("what is your name"), containsString("hello"));
-    }
+    // @Test
+    // public void knowsAboutShakespeare() throws Exception {
+    //     assertThat(queryProcessor.process("what is your name"), containsString("hello"));
+    // }
+
+    // @Test
+    // public void isNotCaseSensitive() throws Exception {
+    //     assertThat(queryProcessor.process("what is your name"), containsString("hello"));
+    // }
 
     @Test
-    public void isNotCaseSensitive() throws Exception {
-        assertThat(queryProcessor.process("what is your name"), containsString("hello"));
+    public void plusWorks() throws Exception {
+        assertThat(queryProcessor.process("what is 9 plus 12"), containsString("21"));
     }
+
+    // @Test
+    // public void maxWorks() throws Exception {
+    //     assertThat(queryProcessor.process("which of the following numbers is the largest:%20905,%2094"), containsInt(20905));
+    // }
 
 }
